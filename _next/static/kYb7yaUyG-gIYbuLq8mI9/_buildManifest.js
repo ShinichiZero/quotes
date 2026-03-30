64b0652ec07a427264be7ff16b057e74acb5d2b5
ShinichiZero <107035614+ShinichiZero@users.noreply.github.com>
2026-03-30 22:18:44 +0000

    Deploying to gh-pages from @ ShinichiZero/quotes@76edc5707dc60d549ee1f3c1b6d8f6414ed3915e 🚀

---
+++ _next/static/kYb7yaUyG-gIYbuLq8mI9/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/quotes//_next/:path+",
+        "destination": "/quotes/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
